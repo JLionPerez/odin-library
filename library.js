00e592849da95a9b2bf7de606ab1e83cc18989b5
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-09-04 14:18:03 -0700

    Add display info of each book on table

--- library.js
+++ library.js
@@ -11,9 +11,9 @@ function Book(id, title, author, num_pages, read_status) {
     this.num_pages = num_pages;
     this.read_status = read_status;
     
-    this.info = () => {
-        return ("ID: " + id + ", " + title + " by " + author + ", " + num_pages + ", " + read_status)
-    }
+    // this.info = () => {
+    //     return ("ID: " + id + ", " + title + " by " + author + ", " + num_pages + ", " + read_status)
+    // }
 }
 
 function addBookToLibrary(title, author, num_pages, read_status) {
@@ -27,3 +27,21 @@ addBookToLibrary("2", "2", "2", "2")
 addBookToLibrary("blah", "blahblah", "blah","blah")
 addBookToLibrary("blah1", "blahblah1", "blah1","blah1")
 
+// console.log(myLibrary[0].info())
+
+function populateTable() {
+    const tbody = document.querySelector("tbody")
+    for (let book of myLibrary) {
+        const tr = document.createElement("tr")
+        for (const val in book) {
+            if (book.hasOwnProperty(val)) {
+                const td = document.createElement("td")
+                td.textContent = book[val]
+                tr.appendChild(td)
+            }
+        }
+        tbody.appendChild(tr)
+    }
+}
+
+populateTable()
